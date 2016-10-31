fdf87916de650ecdfdc49ea04ec638a14abe4a30
veenaw <vwagle2@gmail.com>
2016-10-31 10:33:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/downloadmanager/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.android.downloadmanager;
+
+import android.app.Activity;
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    private long enqueue;
+    private DownloadManager dm;
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BroadcastReceiver receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                if (DownloadManager.ACTION_DOWNLOAD_COMPLETE.equals(action)) {
+                    long downloadId = intent.getLongExtra(
+                            DownloadManager.EXTRA_DOWNLOAD_ID, 0);
+                    DownloadManager.Query query = new DownloadManager.Query();
+                    query.setFilterById(enqueue);
+                    Cursor c = dm.query(query);
+                    if (c.moveToFirst()) {
+                        int columnIndex = c
+                                .getColumnIndex(DownloadManager.COLUMN_STATUS);
+                        if (DownloadManager.STATUS_SUCCESSFUL == c
+                                .getInt(columnIndex)) {
+
+                            System.out.println("DOWNLOAD COMPLETED");
+                        }
+                    }
+                }
+            }
+        };
+
+        registerReceiver(receiver, new IntentFilter(
+                DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+    }
+    public void onClick(View view) {
+        dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+        DownloadManager.Request request = new DownloadManager.Request(
+                Uri.parse("http://mymp3singer.net/files/download/id/8747"));
+       // request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI);
+       request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE);
+        request.setDestinationInExternalFilesDir(this, Environment.DIRECTORY_DOWNLOADS,"Chand Sitare.mp3");
+        enqueue = dm.enqueue(request);
+
+    }
+
+    public void showDownload(View view) {
+        Intent i = new Intent();
+        i.setAction(DownloadManager.ACTION_VIEW_DOWNLOADS);
+        startActivity(i);
+    }
+}
